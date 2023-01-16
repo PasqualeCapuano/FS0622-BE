355504857d92505390fc43e1151b8782b3b570a0
Pascal <79946292+PasqualeCapuano@users.noreply.github.com>
2023-01-16 15:34:03 +0100

    exedrcise final

--- week2/src/day1/Esercizio1.java
+++ week2/src/day1/Esercizio1.java
@@ -6,52 +6,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Esercizio1 {
-	
-	
+
 	public static void main(String[] args) {
-		create();				
+		create();
 	}
 
 	public static void create() {
-		
-		
+
+		int[] numbers = new int[5];
+
+		Random numR = new Random();
+
+		int position = 0;
+		int newValue = 0;
+
 		try {
-			int[] numbers = new int[5];
-			
-			Random numR = new Random();
-						
 			for (int i = 0; i < numbers.length; i++) {
-				numbers[i] = numR.nextInt(11);
+				numbers[i] = numR.nextInt(1, 11);
 			}
 			
-			int position = 0;
-			int newValue = 0;
-
 			System.out.println("- Array iniziale");
 			System.out.println(Arrays.toString(numbers));
 			System.out.println("------------------------------------------");
+			do {
+				System.out.println("- Che numero vuoi aggiungere?");
+				Scanner val = new Scanner(System.in);
+				newValue = val.nextInt();
 
-			System.out.println("- Che numero vuoi aggiungere?");
-			Scanner val = new Scanner(System.in);
-			newValue = val.nextInt();
+				System.out.println("- In che indice dell'array?");
+				Scanner pos = new Scanner(System.in);
+				position = pos.nextInt();
 
-			System.out.println("- In che indice dell'array?");
-			Scanner pos = new Scanner(System.in);
-			position = pos.nextInt();
+				if (newValue > 0) {
+					for (int i = numbers.length - 1; i > position; i--) {
+						numbers[i] = numbers[i];
+					}
 
-			if (newValue > 0) {
-				for (int i = numbers.length - 1; i > position; i--) {
-					numbers[i] = numbers[i - 1];
+					numbers[position] = newValue;
+					System.out.println("- Nuovo Array: " + Arrays.toString(numbers));
+					System.out.println("------------------------------------------");
+				} else {
+					System.out.println("Numero troppo basso, devi inserire un numero maggiore di zero");
+					val.close();
 				}
-				
+			} while (newValue != 0);
 
-				numbers[position] = newValue;
-				System.out.println("- Nuovo Array: " + Arrays.toString(numbers));
-
-			} else {
-				System.out.println("Numero troppo basso, devi inserire un numero maggiore di zero");
-				val.close();
-			}
 		} catch (InputMismatchException e) {
 			System.out.println("Formato non corretto, inserisci un numero valido");
 		} catch (ArrayIndexOutOfBoundsException e) {
